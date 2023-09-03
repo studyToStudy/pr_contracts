55615d576373f0159ea33d97e7d646778536183f
junghoo <jenghoo515@gmail.com>
2023-09-03 17:36:27 +0900

    practice

--- contracts/counter.sol
+++ contracts/counter.sol
@@ -2,17 +2,17 @@
 pragma solidity 0.8.21;
 
 contract Counter {
-    uint public count;
-
-
-    function get() public view returns (uint) {
+    int public count;
+    
+    // get count
+    function get() public view returns (int) {
         return count;
     }
-
+    // increase count
     function inc() public {
         count += 1;
     }
-
+    // decrease count
     function dec() public {
         count -= 1;
     }
