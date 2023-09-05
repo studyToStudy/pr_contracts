51579c3b9af1b15c2bdcb2fee84a8127e4e0d2bc
junghoo <jenghoo515@gmail.com>
2023-09-05 15:05:03 +0900

    practice

---
+++ contracts/constants.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+contract Constants {
+    // Constants are variables that cannot be modified.
+    // Their value is hard coded and using constants can save gas cost
+
+    address public constant MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+    uint256 public constant MY_UNIT = 123;
+}
