560025aa563cc54f6164eb017c2120a8e123a868
junghoo <jenghoo515@gmail.com>
2023-09-05 15:38:24 +0900

    practice

---
+++ contracts/etherAndwei.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+contract EtherUnits {
+    uint256 public oneWei = 1 wei;
+    // 1 wei is equal to 1
+    bool public isOneWel = 1 wei == 1;
+
+
+    /*
+    
+    */
+
+    uint256 public  ontEther = 1 ether;
+    //  1 ther is equal to 10^18 wei
+    bool public isOneEther = 1 ether == 1e18;
+}
